05c3d13b22bdf3112d1d4b2ba187c48065a37f06
frederik88917 <noreply>
2015-09-22 15:56:43 +0000

    Add component add-kid

---
+++ ui/add-kid.reel/add-kid.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/add-kid.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class AddKid
+ * @extends Component
+ */
+exports.AddKid = Component.specialize(/** @lends AddKid# */ {
+    constructor: {
+        value: function AddKid() {
+            this.super();
+        }
+    }
+});
